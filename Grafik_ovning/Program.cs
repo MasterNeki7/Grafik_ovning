5c7a8d47185df7e15605ecaa9cce1d096d492703
Neo Babic <neo.babic@elev.ga.ntig.se>
2022-10-13 14:59:57 +0200

    grafik_bing_qi_ling

---
+++ Grafik_ovning/Program.cs
@@ -0,0 +1,42 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "grafiktest");
+Raylib.SetTargetFPS(60);
+
+Color hotPink = new Color(255, 105, 180, 255);
+Color whitepink = new Color(245, 160, 233, 255);
+
+Rectangle playerRect = new Rectangle(0, 0, 64, 64);
+
+while (Raylib.WindowShouldClose() == false)
+{
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+    {
+        playerRect.x += 2;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+    {
+        playerRect.y += 2;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+    {
+        playerRect.x -= 2;
+    }
+    else if ((Raylib.IsKeyDown(KeyboardKey.KEY_W)))
+    {
+        playerRect.y -= 2;
+    }
+
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(hotPink);
+
+    Raylib.DrawRectangleRec(playerRect, whitepink);
+
+
+
+    Raylib.EndDrawing();
+}
+
+
+
